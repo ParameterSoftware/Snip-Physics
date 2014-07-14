323eb38829fe4d55e951126aba763e0ff1af1eb9
Geroge <Spartan322@live.com>
2014-07-14 12:43:02 -0400

    Create sp_UBase.hpp

---
+++ Snip-Physics/src/Units/sp_UBase.hpp
@@ -0,0 +1,17 @@
+#ifndef SP_UBASE
+#ifndef sp_ubase
+#define SP_UBASE
+#define sp_ubase
+
+class sp_UBase {
+	protected:
+		static double base = 1;
+		double GetBase();
+		double amount;
+	public:
+		sp_UBase(double amount);
+		double Convert(sp_UBase);
+};
+
+#endif
+#endif
